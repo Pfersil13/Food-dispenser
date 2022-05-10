e5b7ca12285ac5efe36908ee8b8313ebc611b0c7
Pablo <75831930+Pfersil13@users.noreply.github.com>
2022-05-10 08:40:00 +0200

    Create Secrets.h

---
+++ src/Secrets.h
@@ -0,0 +1,17 @@
+#ifndef SECRETS_H
+    #define SECRETS_H
+
+    #include <Arduino.h>
+    
+    // Wifi: SSID and password
+    #define WIFI_SSID  "SSID"
+    #define WIFI_PASSWORD "PASSWORD"
+
+    //MQTT Stuff
+    #define MQTT_SERVER_IP "192.168.1.xxx"  //Or your MQTT Server IP / DNS 
+    #define MQTT_USER  "MQTT USER NAME"     
+    #define MQTT_PASSWORD  "MQTT PASSWORD" 
+
+    //Others secrets...
+
+#endif
